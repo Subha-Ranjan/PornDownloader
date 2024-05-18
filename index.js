0a4c4c83a478c34158166d5cc2dbf33e1620ad95
Subha Ranjan <subharanjanbhowmick@gmail.com>
2024-05-19 05:24:57 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+
+const spankbang = require('spankbang')
+const HttpProxyAgent = require('http-proxy-agent')
+const { PornHub } = require('pornhub.js')
+const pornhub = new PornHub()
+
+// const {HttpsProxyAgent} = require('https-proxy-agent')
+// const proxy = 'http://168.63.76.32:3128'
+// const httpsAgent = new HttpsProxyAgent(proxy)
+// pornhub.setAgent(httpsAgent)
+
+
+const xvideos = require('xvideosx');
+ 
+
+async function calApiXvideos(){
+console.log("Xvideos API call");
+const url = 'https://www.xvideos.com/video.udubmed29e7/her_nipples_almost_exploded_-_cumslut_cutie_gets_massive_cumshower';
+const details = await xvideos.videos.details({url});
+console.log(details.files)
+}
+
+async function callApiPornHub(){
+    console.log("Pornhub API Call");
+
+    const url = "https://www.pornhub.com/view_video.php?viewkey=654a393e414f2"
+    pornhub.video(url).then((data)=>console.log(data)).catch((err)=>console.log("So ther is some error: \n",err))
+   
+    console.log(url) // first video
+
+}
+
+async function callApiSpankBang(){
+ console.log("SpankBang api all")
+const url = 'https://spankbang.com/7slau/video/new+sensations+my+girlfriend+calls+me+while+she+s+fucking+bbc+alyx+star'
+const details = await spankbang.videos.details({url})
+
+console.log(details.files)
+}
+
+app.get("/", (req,res)=>{res.status(200).send("Server is up and running!!! 🔥")})
+
+app.listen("8000",()=>{console.log("Server started at 8000"); callApiSpankBang()})
