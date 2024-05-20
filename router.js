d5233787e12ba97d731c2589b4ce45a2e115a53f
Subha Ranjan <subharanjanbhowmick@gmail.com>
2024-05-20 20:17:27 +0530

    backend updated: fixed url issues again

--- router.js
+++ router.js
@@ -67,14 +67,16 @@ router.post("/", async (req, res) => {
 
   //   console.log(details);
   //   res.send("Xv");
-  // } else if (url.includes("spankbang")) {
-  //   console.log("Spankbang API calling");
-  //   const details = await spankbang.videos.details({ url });
-  //   let videoDetails = { platform:' Spankbang',title:details.name, duration:details.duration,image:details.thumbNail, files: [...details.files]};
-  //   // videoDetails=videoDetails.filter(a=> a.quality!=='hls' && a.url !=='')
-  //   console.log(details)
-  //   res.send("Spank");
-  // } else {
+  // }
+   else if (url.includes("spankbang")) {
+    // console.log("Spankbang API calling");
+    // const details = await spankbang.videos.details({ url });
+    // let videoDetails = { platform:' Spankbang',title:details.name, duration:details.duration,image:details.thumbNail, files: [...details.files]};
+    
+    // console.log(details)
+    res.send("Spank");
+  } 
+  // else {
   //   res.send("Invalid Link");
   // }
 });
